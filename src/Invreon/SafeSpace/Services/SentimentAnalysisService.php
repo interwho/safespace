dac1618bb8e416eddcfb7600199ee40fe103a962
Cheng Peng <cwpeng@uwaterloo.ca>
2015-11-07 20:25:10 -0500

    Fixed and removed some code and used urlencode

--- src/Invreon/SafeSpace/Services/SentimentAnalysisService.php
+++ src/Invreon/SafeSpace/Services/SentimentAnalysisService.php
@@ -12,48 +12,40 @@ class SentimentAnalysisService
      */
     public function isPositiveText($text)
     {
-        $text = $this->makeTextUrlFriendly($text);
-        $request = Request::get(BASE_HAVEN_URL . '&text=' . $text);
-
-//        $request = new HttpRequest(BASE_HAVEN_URL, HttpRequest::METH_GET);
-//        $request->addQueryData(array('text' => $text));
+        $request = Request::get(BASE_HAVEN_URL . '&text=' . urlencode($text));
 
         try {
-//            $request->send();
             $response = $request->send();
             if ($response->code == 200) {
                 return $this->isPositiveResponse($response->body);
             } else {
                 return false;
             }
-//            if ($request->getResponseCode() == 200) {
-//                return $this->isPositiveResponse($request->getResponseBody());
-//            } else {
-//                return false;
-//            }
         } catch (Exception $ex) {
             echo $ex;
         }
     }
 
     /**
-     * @param $text
-     * @return string
-     */
-    private function makeTextUrlFriendly($text)
-    {
-        return strtolower(trim(preg_replace('/[^a-zA-Z0-9]+/', '-', $text), '-'));
-    }
-
-    /**
      * @param $responseBody
      * @return boolean
      */
-    private function isPositiveResponse($responseBody) {
+    private function isPositiveResponse($responseBody)
+    {
+        $positiveResults = $responseBody->positive;
         $negativeResults = $responseBody->negative;
+        $score = $responseBody->aggregate->score;
 
         if (!empty($negativeResults)) {
             return false;
+        }
+
+        if (empty($positiveResults)) {
+            return false;
+        }
+
+        if ($score >= 0) {
+            return true;
         }
 
         return true;
